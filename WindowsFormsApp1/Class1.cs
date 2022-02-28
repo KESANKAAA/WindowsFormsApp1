1832aa08961acf6f86c2799a0d0564d91d198736
KESA <KESA@DESKTOP-85J34EV>
2022-02-28 20:20:47 +0400

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+     class Class1
+    {
+        public void schet1()
+        {
+
+        }
+
+        public void schet2()
+        {
+
+        }
+
+        public void schet3()
+        {
+
+        }
+
+        public void schet4()
+        {
+
+        }
+
+        public void schet5()
+        {
+
+        }
+
+        public void file()
+        {
+            string path = @"C:\Users\student.ISTU\";
+            FileStream fs = File.Create(path);
+        }
+    }
+}
